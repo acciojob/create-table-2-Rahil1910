41be2c9ba2f457dd28605a4b5cce4db13484249c
Rahil1910 <rahilrafique1234@gmail.com>
2025-01-29 13:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-var rn = 0;
-    var cn = 0;
-    function closeModal() {
-	    var myModal1 = new bootstrap.Modal(document.getElementById('exampleModal'));
-	    myModal1.hide();
-    }
-    function openSecondModal() {
-	    var myModal2 = new bootstrap.Modal(document.getElementById('exampleModal2'));
-	    myModal2.show();
-        rn = document.getElementById('row-input').value;
-    }
-    function createTable() {
-        //Write your code here
-        const table = document.querySelector('#myTable');
-        cn = document.getElementById('col-input').value;
-        for(let i=1;i<=rn;i++){
-            const row = document.createElement('tr');
-            for(let j=1;j<=cn;j++){
-                const cell = document.createElement('td');
-                cell.textContent = `Row-${i-1} Column-${j-1}`;
-                row.appendChild(cell);
-            }
-            table.appendChild(row);
-        }
-    }
+function insert_Row() {
+	let table = document.getElementById('sampleTable');
+    let newRow = table.insertRow(0);
+
+    let cell1 = newRow.insertCell(0); 
+    let cell2 = newRow.insertCell(1); 
+
+    cell1.innerHTML = 'New Cell1';
+    cell2.innerHTML = 'New Cell2';
+}
